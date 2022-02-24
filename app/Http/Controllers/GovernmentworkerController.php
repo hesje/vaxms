27c89fa869ebfaa47a7ceb2b6ff547c56f45874e
roglasius <99327678+roglasius@users.noreply.github.com>
2022-02-24 15:53:34 +0100

    vaccination schedule + started on government worker thingies

---
+++ app/Http/Controllers/GovernmentworkerController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Governmentworker;
+use App\Http\Requests\StoreGovernmentworkerRequest;
+use App\Http\Requests\UpdateGovernmentworkerRequest;
+
+class GovernmentworkerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreGovernmentworkerRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreGovernmentworkerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Governmentworker  $governmentworker
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Governmentworker $governmentworker)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Governmentworker  $governmentworker
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Governmentworker $governmentworker)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateGovernmentworkerRequest  $request
+     * @param  \App\Models\Governmentworker  $governmentworker
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateGovernmentworkerRequest $request, Governmentworker $governmentworker)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Governmentworker  $governmentworker
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Governmentworker $governmentworker)
+    {
+        //
+    }
+}
